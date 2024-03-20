c5568540ba147f431f92b879e7e525775d036a90
Matheo <matheomarumo@gmail.com>
2024-03-19 21:30:09 -0300

    Dragon movement and change in terrain

--- Assets/Scripts/Player Controller.cs
+++ Assets/Scripts/Player Controller.cs
@@ -8,6 +8,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float xRange = 10f;
     [SerializeField] float yRange = 10f;
 
+    [SerializeField] float positionPitchFactor = -1.5f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float controlYawFactor = 0.8f;
+    [SerializeField] float controlRollFactor = -15f;
+
+    float horizontalMovement, verticalMovement;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +24,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal");
-        float verticalMovement = Input.GetAxis("Vertical");
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + verticalMovement * controlPitchFactor;
+        float yaw = transform.localPosition.x * controlYawFactor;
+        float roll = horizontalMovement * controlRollFactor;
+
+        Debug.Log("Rotation: " + Quaternion.Euler(pitch, yaw, roll));
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
 
         float newXPos = transform.localPosition.x + horizontalMovement * Time.deltaTime * movementSpeed;
         float newYPos = transform.localPosition.y + verticalMovement * Time.deltaTime * movementSpeed;
@@ -27,12 +49,13 @@ public class PlayerController : MonoBehaviour
         newXPos = Mathf.Clamp(newXPos, -xRange, xRange);
         newYPos = Mathf.Clamp(newYPos, -yRange, yRange);
 
+        Debug.Log("Vertical Movement: " + verticalMovement);
+
         transform.localPosition = new Vector3
         {
             x = newXPos,
             y = newYPos,
             z = transform.localPosition.z
         };
-
     }
 }

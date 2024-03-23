398c5babbcde419aef843e318d7c973d0923175c
Matheo <matheomarumo@gmail.com>
2024-03-23 20:58:28 -0300

    Destroy on particle collision

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -7,6 +7,7 @@ public class PlayerCollision : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collision detected: "+ this.name + " " + collision.gameObject.name);
+        Destroy(gameObject);
     }
 
     void OnTriggerEnter(Collider other)

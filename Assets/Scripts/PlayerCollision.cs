6634fbaca54d45243b2d9e331109e6a2386d4a9d
Matheo <matheomarumo@gmail.com>
2024-03-25 19:30:00 -0300

    Explode and reload after collision

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,15 +4,23 @@ using UnityEngine.SceneManagement;
 public class PlayerCollision : MonoBehaviour
 {
     [SerializeField] float reloadTime = 1f;
+    [SerializeField] ParticleSystem explosionParticles;
 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger: "+ gameObject.ToString() + " trigered by " + other.gameObject.ToString());
-        GetComponent<PlayerController>().enabled = false;
-        Invoke("CrashSequence", reloadTime);
+        StartCrashSequence();
     }
 
-    void CrashSequence()
+    void StartCrashSequence()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<PlayerController>().enabled = false;
+        explosionParticles.Play();
+        Invoke("ReloadScene", reloadTime);
+    }
+
+    void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }

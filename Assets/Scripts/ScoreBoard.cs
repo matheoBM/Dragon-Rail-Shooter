d66d64a7035d6b081e5c3181c738ae5038def028
Matheo <matheomarumo@gmail.com>
2024-03-26 20:46:43 -0300

    Initial UI for scoring

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     int score;
+    TMP_Text scoreText;
+
+    private void Start()
+    {
+        scoreText = GetComponent<TMP_Text>();
+        scoreText.text = score.ToString();
+    }
 
     public void IncreaseScore(int newPoints)
     {
         score += newPoints;
+        scoreText.text = score.ToString();
         Debug.Log("Score: " + score);
     }
 }

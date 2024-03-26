ca2a5fdb65627b2761904c213329e30f4c97105a
Matheo <matheomarumo@gmail.com>
2024-03-26 06:59:24 -0300

    Scoring

--- Assets/Scripts/EnemyCollision.cs
+++ Assets/Scripts/EnemyCollision.cs
@@ -6,8 +6,27 @@ public class EnemyCollision : MonoBehaviour
 {
     [SerializeField] GameObject explosionParticles;
     [SerializeField] Transform parentTransform;
+    [SerializeField] int enemyScore = 10;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
 
     void OnParticleCollision(GameObject other)
+    {
+        ProecessHit();
+        KillEnemy();
+    }
+
+    void ProecessHit()
+    {
+        scoreBoard.IncreaseScore(enemyScore);
+    }
+
+    void KillEnemy()
     {
         GameObject explosion = Instantiate(explosionParticles, transform.position, Quaternion.identity);
         explosion.transform.parent = parentTransform;
